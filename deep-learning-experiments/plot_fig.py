91014e3fe69177663e2c9914645796625a410975
Jiarui Li <118020229@link.cuhk.edu.cn>
2023-11-13 14:58:24 +0000

    script: plot fig

--- deep-learning-experiments/plot_fig.py
+++ deep-learning-experiments/plot_fig.py
@@ -8,9 +8,10 @@ import matplotlib
 
 
 topologies = [
+    "Solo",
     "Star",
     "Ring",
-    "Fully Connected",
+    # "Fully Connected",
 ]
 
 dataset = "CIFAR-10"
@@ -19,19 +20,22 @@ sampling_algos = [
     "DRR",
 ]
 
-base_path = "/home/ubuntu/Desktop/experiments/lr_speedup"
-output_path = "/home/ubuntu/Desktop/experiments/lr_speedup/figures"
+exp_dir = "/home/ubuntu/Desktop/topology-in-decentralized-learning/experiments"
+base_path = f"{exp_dir}/epoch100"
+output_path = f"{exp_dir}/epoch100/figures"
 
 learning_rates = [
-    0.001, 0.005, 0.01, 0.05,
+    0.001, 0.005, 0.01, 0.05, 0.1, 0.5
 ]
 losses = [
-    ("Star", "DSGD", [1.817,  1.350, 1.243, 1.506]),
-    ("Ring", "DSGD", [1.757, 1.022, 0.813, 0.656]),
-    ("Fully Connected", "DSGD", [1.703, 0.845, 0.628, 0.426]),
-    ("Star", "DRR", [1.831, 1.369, 1.257, 1.523]),
-    ("Ring", "DRR", [1.755, 1.032, 0.815, 0.640]),
-    ("Fully Connected", "DRR", [1.703, 0.846, 0.616, 0.432]),
+    ("Solo", "DSGD", [1.204, 0.921, 1.025, 2.051, 2.308, 2.304]), 
+    ("Star", "DSGD", [1.053, 0.617, 0.566, 0.687, 2.306, 2.303]), # the last two diverges
+    ("Ring", "DSGD", [0.927, 0.484, 0.458, 0.418, 0.432, 2.304]),
+    ("Fully Connected", "DSGD", [0.845, 0.502, 0.483, 0.503, 0.444, 2.303]),
+    ("Solo", "DRR", [1.206, 0.928, 0.974, 2.159, 2.315, 2.303]), # the last three diverges
+    ("Star", "DRR", [1.047, 0.589, 0.536, 0.661, 2.018, 2.303]), # the last two diverges
+    ("Ring", "DRR", [0.922, 0.474, 0.440, 0.456, 0.423, 2.306]),
+    ("Fully Connected", "DRR", [0.845, 0.500, 0.529, 0.490, 0.445, 2.303]),
 
 ]
 
@@ -43,8 +47,8 @@ def plot_loss():
         fig = plt.figure()
         plt.clf()
         ax = fig.add_subplot(111)
-        ax.set_xlabel("Loss")
-        ax.set_ylabel("Learning Rate")
+        ax.set_xlabel("Learning Rate")
+        ax.set_ylabel("Loss")
         ax.set_title("Loss vs Learning Rate for {}".format(sampling_algo))
         ax.set_xscale("log")
         # ax.set_yscale("log")
